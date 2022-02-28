a9c1f3a893510ffebc4000b647962dcc1753ef86
jsantore <jsantore@bridgew.edu>
2022-02-28 14:45:12 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module GUIWork1
+
+go 1.17
+
+require github.com/blizzy78/ebitenui v0.0.0-20211114161546-ca1a302d930b
+
+require (
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211024062804-40e447a793be // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.2.2 // indirect
+	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	golang.org/x/exp v0.0.0-20211111183329-cb5df436b1a8 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/mobile v0.0.0-20211109191125-d61a72f26a1a // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211113001501-0c823b97ae02 // indirect
+)
---
+++ windowWork.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"embed"
+	"github.com/blizzy78/ebitenui"
+	"github.com/blizzy78/ebitenui/image"
+	"github.com/blizzy78/ebitenui/widget"
+	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/font/basicfont"
+	"image/color"
+	"image/png"
+	"log"
+)
+
+//go:embed graphics/*
+var EmbeddedAssets embed.FS
+
+func main() {
+	ebiten.SetWindowSize(900, 800)
+	ebiten.SetWindowTitle("Minimal Ebiten UI Demo")
+
+	demoApp := GuiApp{AppUI: MakeUIWindow()}
+
+	err := ebiten.RunGame(&demoApp)
+	if err != nil {
+		log.Fatalln("Error running User Interface Demo", err)
+	}
+}
+
+func (g GuiApp) Update() error {
+	//TODO finish me
+	g.AppUI.Update()
+	return nil
+}
+
+func (g GuiApp) Draw(screen *ebiten.Image) {
+	//TODO finish me
+	g.AppUI.Draw(screen)
+}
+
+func (g GuiApp) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+type GuiApp struct {
+	AppUI *ebitenui.UI
+}
+
+func MakeUIWindow() (GUIhandler *ebitenui.UI) {
+	background := image.NewNineSliceColor(color.Gray16{})
+	rootContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewGridLayout(
+			widget.GridLayoutOpts.Columns(1),
+			widget.GridLayoutOpts.Stretch([]bool{true}, []bool{false, true, false}),
+			widget.GridLayoutOpts.Padding(widget.Insets{
+				Top:    20,
+				Bottom: 20,
+			}),
+			widget.GridLayoutOpts.Spacing(0, 20))),
+		widget.ContainerOpts.BackgroundImage(background))
+	textInfo := widget.TextOptions{}.Text("This is our first Window", basicfont.Face7x13, color.White)
+
+	idle, err := loadImageNineSlice("button-idle.png", 20, 0)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	hover, err := loadImageNineSlice("button-hover.png", 20, 0)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	pressed, err := loadImageNineSlice("button-pressed.png", 20, 0)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	disabled, err := loadImageNineSlice("button-disabled.png", 20, 0)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	buttonImage := &widget.ButtonImage{
+		Idle:     idle,
+		Hover:    hover,
+		Pressed:  pressed,
+		Disabled: disabled,
+	}
+	button := widget.NewButton(
+		// specify the images to use
+		widget.ButtonOpts.Image(buttonImage),
+		// specify the button's text, the font face, and the color
+		widget.ButtonOpts.Text("Press Me", basicfont.Face7x13, &widget.ButtonTextColor{
+			Idle: color.RGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+		// specify that the button's text needs some padding for correct display
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Left:  30,
+			Right: 30,
+		}),
+		// ... click handler, etc. ...
+	)
+	rootContainer.AddChild(button)
+	rootContainer.AddChild(widget.NewText(textInfo))
+	GUIhandler = &ebitenui.UI{Container: rootContainer}
+	return GUIhandler
+}
+
+func loadImageNineSlice(path string, centerWidth int, centerHeight int) (*image.NineSlice, error) {
+	i := loadPNGImageFromEmbedded(path)
+
+	w, h := i.Size()
+	return image.NewNineSlice(i,
+			[3]int{(w - centerWidth) / 2, centerWidth, w - (w-centerWidth)/2 - centerWidth},
+			[3]int{(h - centerHeight) / 2, centerHeight, h - (h-centerHeight)/2 - centerHeight}),
+		nil
+}
+
+func loadPNGImageFromEmbedded(name string) *ebiten.Image {
+	pictNames, err := EmbeddedAssets.ReadDir("graphics")
+	if err != nil {
+		log.Fatal("failed to read embedded dir ", pictNames, " ", err)
+	}
+	embeddedFile, err := EmbeddedAssets.Open("graphics/" + name)
+	if err != nil {
+		log.Fatal("failed to load embedded image ", embeddedFile, err)
+	}
+	rawImage, err := png.Decode(embeddedFile)
+	if err != nil {
+		log.Fatal("failed to load embedded image ", name, err)
+	}
+	gameImage := ebiten.NewImageFromImage(rawImage)
+	return gameImage
+}
+
+func FunctionNameHere(args *widget.ButtonClickedEventArgs) {
+	//do your work here
+}
